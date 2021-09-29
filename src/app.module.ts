bce0e1f80ac179f5410d7e9d5e83c9ec9cc60ff6
Arnod Chirivi Pinzon <arnod.chirivi@gmail.com>
2021-09-29 00:09:30 -0500

    product service is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { OrdersController } from './controlers/orders/orders.controller';
 import { UsersController } from './controlers/users/users.controller';
 import { CostumersController } from './controlers/costumers/costumers.controller';
 import { BrandsController } from './controlers/brands/brands.controller';
+import { ProductsService } from './services/products/products.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, CostumersController, BrandsController],
-  providers: [AppService],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
